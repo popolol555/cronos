f3ccde4a662c5818ec531cb4ca91f5e7a5b29bd2
popolol555 <kira.riuk@gmail.com>
2025-07-10 15:49:02 -0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'cronometro-cache-v1';
-const urls = ['/', '/index.html', '/manifest.json', '/service-worker.js', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
